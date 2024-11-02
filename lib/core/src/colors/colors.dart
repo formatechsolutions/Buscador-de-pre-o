3f63e4ce4004a29793e34ade0bf6fd219cf4d55f
brenoluizdev <brenoluizpv3@gmail.com>
2024-11-02 13:08:32 -0300

    ajustes e componentização dos textos

--- lib/core/src/colors/colors.dart
+++ lib/core/src/colors/colors.dart
@@ -13,4 +13,5 @@ class ColorsConfig {
   static const Color backgroundWhite = Color(0xFFFFFFFF);
   static const Color backgroundBlack = Color.fromARGB(255, 0, 0, 0);
   static const Color primary = Color(0xFF353668);
+  static const Color transparentGrey = Color.fromARGB(255, 159, 159, 160);
 }
