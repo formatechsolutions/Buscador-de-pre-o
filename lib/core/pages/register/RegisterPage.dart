57dcde5af0ce188ee33a7839466455193df6cac8
brenoluizdev <brenoluizpv3@gmail.com>
2024-11-03 22:14:30 -0300

    ajustando erros de sintaxe

--- lib/core/pages/register/RegisterPage.dart
+++ lib/core/pages/register/RegisterPage.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: use_key_in_widget_constructors
+// ignore_for_file: use_key_in_widget_constructors, file_names
 
 import 'package:busca_preco/core/pages/custom/appBar/simple_app_bar.dart';
 import 'package:busca_preco/core/pages/custom/colors.dart';
