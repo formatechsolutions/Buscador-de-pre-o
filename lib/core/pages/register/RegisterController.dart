685f001cab7a38dcbfcfae1a7614962963dd66fc
brenoluizdev <brenoluizpv3@gmail.com>
2024-11-04 20:30:32 -0300

    form de registro

--- lib/core/pages/register/RegisterController.dart
+++ lib/core/pages/register/RegisterController.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class Registercontroller {
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
- 
+
+  Rx<bool> showPassword = false.obs;
+
   final TextEditingController nomeController = TextEditingController();
   final TextEditingController emailController = TextEditingController();
   final TextEditingController senhaController = TextEditingController();
@@ -13,5 +16,9 @@ class Registercontroller {
       print('Email: ${emailController.text}');
       print('Senha: ${senhaController.text}');
     }
+  }
+
+  void showPasswordToggle() {
+    showPassword.value = !showPassword.value;
   }
 }
