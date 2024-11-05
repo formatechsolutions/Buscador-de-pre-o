9d5f8e946f2b41a62673afb9c50e0db16bceef51
brenoluizdev <brenoluizpv3@gmail.com>
2024-11-05 10:05:53 -0300

    removendo colors do services

--- lib/core/services/services_manager.dart
+++ lib/core/services/services_manager.dart
@@ -1,12 +1,9 @@
 import 'dart:async';
-import 'package:busca_preco/core/pages/custom/colors_controller.dart';
-import 'package:get/get.dart';
 
 class ServicesManager {
   static final servicesInitialized = Completer<void>();
 
   static Future<void> init() async {
-    Get.put(ColorsController());
 
     if (!servicesInitialized.isCompleted) {
       servicesInitialized.complete();
